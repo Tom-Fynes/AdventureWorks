10c7b91cc8361afbd48ac1b955faf156b112bbfa
Tom Fynes <Thomas.fynes@emishealth.com>
2020-02-19 11:59:20 +0000

    m

---
+++ AdventureWorks/obj/Debug/postdeploy.sql
@@ -0,0 +1,25 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+IF (EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'PostDeploymentTable'))
+BEGIN
+    DROP TABLE [dbo].[PostDeploymentTable]
+END
+Create Table dbo.PostDeploymentTable
+(
+	TableID int Identity(1,1)Primary Key,
+	SomeDescripttion nVarchar(250)
+)
+GO
